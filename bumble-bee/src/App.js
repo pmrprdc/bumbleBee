dc57d2e6f0845ef06bf280e652bc616aa2985693
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-02 19:46:08 -0500

    created a styled log-in button

--- bumble-bee/src/App.js
+++ bumble-bee/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import styled from "styled-components"
 
+const StyledButton = styled.button`
+background-color: black;
+font-size: 32px;
+border-radius: 5px;
+color: white;`;
 
 
 
 function App() {
+
   return (
-    <h1>BumbleBee</h1>
+    <StyledButton> Log in</StyledButton>
   
   )
 }
